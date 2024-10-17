15d11028feeef8e057b2de41342ec54647102060
caolinqi <3341538119@qq.com>
2024-10-17 16:55:18 +0800

    element-plus的按需引入配置

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,23 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 //配置路径别名
 import path from "path";
+//配置element-plus按需引入
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // ...
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
